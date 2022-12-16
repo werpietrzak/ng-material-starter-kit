702a6b951a8499338d2c03c21bd3f5b1cfec0293
Wer Pietrzak <95078050+werpietrzak@users.noreply.github.com>
2022-12-16 11:35:51 +0100

    feat: add loading and empty state to category checkboxes

--- src/app/components/checkbox-categories/checkbox-categories.component-module.ts
+++ src/app/components/checkbox-categories/checkbox-categories.component-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CheckboxCategoriesComponent } from './checkbox-categories.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
-  imports: [MatCardModule, MatCheckboxModule, CommonModule],
+  imports: [MatCardModule, MatCheckboxModule, CommonModule, MatProgressSpinnerModule],
   declarations: [CheckboxCategoriesComponent],
   providers: [],
   exports: [CheckboxCategoriesComponent]
